db06ed7b53df23a7af7ae8ebae16d6ce079ec534
John Beale <beale@bealecorner.com>
2015-06-15 22:16:56 -0700

    code cleanup

--- util/disk-clean.sh
+++ util/disk-clean.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
-# Disk space cleanup script  J.Beale
+# Disk space cleanup script  J.Beale  15 June 2015
 
 # maximum percent disk use allowed (when exceeded, delete files until not exceeded)
 USELIMIT=60   
@@ -7,18 +7,16 @@ USELIMIT=60
 # directory of images to weed out
 PDIR=/mnt/usb1/pics/full
 
-# returns the % disk space in use on /dev/sda1, sed to remove the trailing '%' symbol
+# returns the percentage disk space in use. sed to remove the trailing '%' symbol
 DISKUSE=$(df $PDIR | tail -1 | awk '{ print $5 }' | sed s'/%$//')
 
 while  [[ $DISKUSE -gt $USELIMIT ]];
  do
   echo Disk usage $DISKUSE % is greater than limit of $USELIMIT
-  # using zsh, find the oldest file
-  OLDEST=$(ls $PDIR/*(.Om[1]))
-  echo Now removing: $OLDEST
-  # sleep 1
+  # using zsh, find the single oldest file
+  # OLDEST=$(ls $PDIR/*(.Om[1]))
+  # echo Now removing: $OLDEST
   rm $PDIR/*(.Om[1])
-  # returns the % disk space in use on /dev/sda1, sed to remove the trailing '%' symbol
   DISKUSE=$(df $PDIR | tail -1 | awk '{ print $5 }' | sed s'/%$//')
  done
  
