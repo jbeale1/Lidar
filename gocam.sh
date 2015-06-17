c81c05b531fd8bb925875a108f265b366759bfaf
John Beale <beale@bealecorner.com>
2015-06-16 20:29:33 -0700

    improve performance with marginal signals (stop continual trigger on noise)

--- gocam.sh
+++ gocam.sh
@@ -6,7 +6,7 @@ sudo killall raspistill
 sudo killall serlog1.py
 
 cd /mnt/usb1/pics
-raspistill -n -ev -4 -t 0 -s -o %03d.jpg &
+raspistill -n -ex sports -ev -4 -t 0 -s -o %03d.jpg &
 #raspistill -n -ex night -roi .15,0,1.0,0.80 -rot 270 -w 800 -h 700 -t 0 -s -o %03d.jpg &
 #raspistill -n -ex night -rot 270 -w 600 -h 800 -t 0 -s -o %03d.jpg &
 #raspistill -ex night -rot 270 -w 800 -h 600 -t 0 -s -o %03d.jpg &
--- serlog1.py
+++ serlog1.py
@@ -9,10 +9,14 @@ addr  = '/dev/ttyACM0'  # serial port to read data from
 baud  = 115200            # baud rate for serial port
 fname = 'LIDAR-log2.csv'   # log file to save data in
 fmode = 'a'             # log file mode = append
-maxRange = 3000         # maximum valid "max-range" reading (larger means some glitch)
+maxRange = 3050         # maximum valid "max-range" reading (larger means some glitch)
+lastRange = 0           # previous range reading
+rangeDeltaMin = 75      # new event must be at least this much different than last
+minInterval = datetime.timedelta(milliseconds=1500)  # minimum interval for events
 
 secMidnight = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
 # secStart = datetime.datetime.now()
+secOld = datetime.datetime.now()   # set previous event to start time
 
 try:
   rPID = subprocess.Popen(["cat", "/tmp/raspi_PID"], stdout=subprocess.PIPE).communicate()[0]
@@ -30,24 +34,37 @@ with serial.Serial(addr,115200) as pt, open(fname,fmode) as outf:
         inline = spb.readline()  # read one line of text from serial port
 
         secNow = datetime.datetime.now()
-        secElapsed = '%.3f' % (secNow - secMidnight).total_seconds()
-        stime = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S') # current time
-        indat = inline.lstrip().rstrip()  # input data
-        if indat != "": # add other needed checks to skip titles
-          cols = indat.split(",")
-          # print( cols)
-          try:
-           rmax = int(cols[4])
-          except:
-           rmax = 0
-          print( rmax )
-        if (rmax < maxRange):
-          try:
-           subprocess.call(["kill", "-s", "SIGUSR1", rPID])  # send capture command to raspistill
-          except:
-           print("Error sending signal to %s." % rPID)
-          x = stime + ", " + secElapsed + ", " + indat + "\n"  # remove trailing whitespace from input
-          print (x,end='')    # echo line of text on-screen
-          outf.write(x)       # write line of text to file
-          outf.flush()        # make sure it actually gets written out
+        if ( 1 ):
+        # if ((secNow - secOld) > minInterval):  # only pay attention if exceeded minInterval
+         indat = inline.lstrip().rstrip()  # input data
+         if indat != "": # add other needed checks to skip titles
+           cols = indat.split(",")
+           # print( cols)
+           try:
+            cm = int(cols[0])
+	    count = int(cols[1])
+            rmin = int(cols[2])
+            ravg = int(cols[3])
+            rmax = int(cols[4])
+           except:
+            rmax = 0
+            count = 0
+           # print( rmax )
+         rangeDelta = cm - lastRange
+         # print(rangeDelta)
+         if (rmax < maxRange) and (cm != 0) and (abs(rangeDelta) > rangeDeltaMin):
+          if ((secNow - secOld) > minInterval):  # only pay attention if exceeded minInterval
+           secOld = secNow  # remember for next time
+           secElapsed = '%.3f' % (secNow - secMidnight).total_seconds()
+           stime = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S') # current time
+
+           lastRange = cm
+           try:
+            subprocess.call(["kill", "-s", "SIGUSR1", rPID])  # send capture command to raspistill
+           except:
+            print("Error sending signal to %s." % rPID)
+           x = stime + ", " + secElapsed + ", " + indat + "\n"  # remove trailing whitespace from input
+           print (x,end='')    # echo line of text on-screen
+           outf.write(x)       # write line of text to file
+           outf.flush()        # make sure it actually gets written out
 
